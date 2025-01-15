0480a34b2badceb9287d7a7e010c56c67ad7957b
kcrypto126 <technt22@gmail.com>
2025-01-16 07:48:40 +1000

    update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -17,16 +17,20 @@ const Header: React.FC = () => {
     navigator.push("/");
 
     setTimeout(() => {
-      setIsButton(false)
+      setIsButton(false);
     }, 600);
-  }
-
+  };
 
   return (
     <>
       <div className="w-full bg-[#02021D] z-50 fixed top-0 left-0">
-        <div className="max-w-[1440px] h-[72px] m-[auto] flex flex-row justify-between items-center relative">
-          <div className="flex flex-1">
+        <div
+          className="max-w-[1440px] h-[72px] m-[auto] flex flex-row justify-between items-center relative bg-no-repeat bg-right-top pl-0 pr-0 sm:pl-[20px] lg:pl-[83px]"
+          style={{
+            backgroundImage: "url('/assets/navbar-back2.png')",
+          }}
+        >
+          <div className="flex flex-1 pl-4 sm:pl-0">
             <Image
               src={"/assets/logo.svg"}
               width={"150"}
@@ -38,7 +42,7 @@ const Header: React.FC = () => {
               }}
             />
           </div>
-          <div className="flex flex-[3] flex-col sm:flex-row border mt-[300px] sm:mt-0 absolute sm:relative w-[100%] gap-4 font-[Oswald] text-[18px] font-bold items-center flex-wrap">
+          <div className=" border flex flex-[3] flex-wrap flex-col sm:flex-row justify-center sm:justify-start items-center absolute top-0 h-[100vh] sm:h-auto sm:relative w-[100%] gap-4 font-[Oswald] text-[18px] font-bold">
             <Link
               href={"/"}
               prefetch
@@ -145,16 +149,16 @@ const Header: React.FC = () => {
               $CHILL
             </Link>
           </div>
-          <div className="flex-1 justify-end items-start relative hidden md:flex">
+          <div className="flex-1 flex justify-center lg:justify-end items-center lg:items-start h-[100vh] lg:h-[100%] w-[100%] absolute lg:relative lg:flex">
             <button
-              className="w-[264px] h-[84px] rounded-md mt-[10px] font-bold font-[Oswald] text-[38px] text-[#020215]"
+              className="w-[264px] h-[84px] rounded-md font-bold font-[Oswald] text-[38px] text-[#020215]"
               style={{
                 backgroundImage: "url(./assets/play-now-btn.png)",
                 backgroundSize: "100% 100%",
                 boxShadow: "0px 0px 20px 3px",
               }}
               onClick={() => {
-                handleButton();                
+                handleButton();
               }}
             >
               PLAY NOW
@@ -165,8 +169,18 @@ const Header: React.FC = () => {
               width={264}
               height={264}
               className=" absolute top-[-70px] left-[-30px] transition-all duration-300"
-              style={{display: isButton == true ? "block" : "none"}}
+              style={{ display: isButton == true ? "block" : "none" }}
             />
+          </div>
+          <div className="pr-4 sm:pr-0 flex-1 text-white flex flex-row justify-end items-center sm:hidden">
+            <button
+              className=" w-9 h-9 bg-center bg-no-repeat"
+              style={{
+                backgroundImage: "url('/assets/menu-open.png')",
+                backgroundSize: "24px 24px",
+              }}
+              onClick={() => {}}
+            ></button>
           </div>
         </div>
       </div>
