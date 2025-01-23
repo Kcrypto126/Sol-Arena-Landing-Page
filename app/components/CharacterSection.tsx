f4e559ecc3abdb7835ba9a1421bfc1b65670a744
kcrypto126 <technt22@gmail.com>
2025-01-23 23:07:17 +1000

    landing page update

--- app/components/CharacterSection.tsx
+++ app/components/CharacterSection.tsx
@@ -25,9 +25,10 @@ const CharacterSection: React.FC = () => {
         <div
           className={` second-section max-w-[1440px] m-auto flex flex-row pl-0 pr-0 xl:pl-[180px] relative bg-no-repeat bg-[left_16px_top_133px] lg:bg-[left_16px_top_120px] xl:bg-[left_180px_top_120px] ${
             isScale == 1
-              ? "bg-[length:287px_103px] lg:bg-[length:363px_130px]"
-              : "bg-[length:90%] md:bg-[length:550px_103px] lg:bg-[length:814px_145px]"
-          }`}
+              ? "bg-[length:90%] md:bg-[length:550px_103px] lg:bg-[length:auto]"
+              : "bg-[length:90%] md:bg-[length:550px_103px] lg:bg-[length:auto]"
+          }
+          `}
           style={{
             backgroundImage:
               isScale == 1
@@ -59,8 +60,14 @@ const CharacterSection: React.FC = () => {
               height={68}
               className={`${
                 isScale == 1
-                  ? " max-w-[150px] md:max-w-[190px] max-h-[53px] md:max-h-[68px]"
-                  : "max-w-[100%] max-h-[53px] md:max-h-[68px] ml-[-7px]"
+                  ? " max-w-[230px] mt-[-20px] sm:mt-auto h-auto sm:max-w-[449px]  sm:max-h-[68px]"
+                  : isScale == 2
+                  ? " max-w-[230px] mt-[-30px] sm:mt-auto h-auto sm:max-w-[449px]  sm:max-h-[68px]"
+                  : isScale == 3
+                  ? " max-w-[230px] mt-[-20px] sm:mt-auto h-auto sm:max-w-[449px]  sm:max-h-[68px]"
+                  : isScale == 4
+                  ? " max-w-[270px] mt-[-40px] sm:mt-[15px] h-auto sm:max-w-[449px]  sm:max-h-[68px]"
+                  : "max-w-[270px] mt-[-40px] sm:mt-[15px] h-auto sm:max-w-[449px]  sm:max-h-[68px]"
               } pl-4 md:pl-8 xl:pl-0 `}
             />
             <div className=" flex flex-row gap-4 pl-4 mt-6 md:pl-8 xl:pl-0">
@@ -255,7 +262,7 @@ const CharacterSection: React.FC = () => {
               alt="character"
               width={580}
               height={730}
-              className="max-w-[343px] max-h-[421px] md:max-w-[580px] md:max-h-[730px] mr-[-25px] md:mr-0 z-0"
+              className="max-w-[343px] max-h-[421px] md:max-w-[580px] md:max-h-[730px] mr-[-25px] md:mr-[20px] z-0"
             />
             <Image
               src={
@@ -272,7 +279,7 @@ const CharacterSection: React.FC = () => {
               alt="character"
               width={345}
               height={486}
-              className=" z-20 max-w-[94px] max-h-[151px] md:max-w-[345px] md:max-h-[486px] absolute bottom-0 md:bottom-[41px] right-0"
+              className=" z-20 max-w-[104px] max-h-[161px] md:max-w-[325px] md:max-h-[466px] absolute bottom-0 md:bottom-[41px] right-0"
             />
           </div>
         </div>
